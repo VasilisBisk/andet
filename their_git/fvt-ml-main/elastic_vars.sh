5f19bc1717de0177c254184b068036256cadab21
VasilisBisk <vmpisketzis@itp.uni-frankfurt.de>
2023-09-16 15:56:48 +0300

    FFT Analysis

---
+++ their_git/fvt-ml-main/elastic_vars.sh
@@ -0,0 +1,4 @@
+export ES_RAW_DATA_REPO_INDEX=1 # Choose an appropriate name for your index or check project docs
+export ES_RAW_DATA_REPO_KEEP_ALIVE=1m  # This is just an example, adjust as needed
+export ES_RAW_DATA_REPO_SIZE=100  # Another example, adjust as required
+
